2c93f985d8c8de4e71a45b2e2e7088ffb7ed5e91
wade <wade-liwei@icloud.com>
2020-10-12 16:09:47 +0800

    deploty to 200

--- edc/networks.js
+++ edc/networks.js
@@ -1,8 +1,16 @@
 module.exports = {
   networks: {
-    development: {
+    development100: {
       protocol: 'http',
       host: '192.168.3.100',
+      port: 8545,
+      gas: 5000000,
+      gasPrice: 5e9,
+      networkId: '*',
+    },
+    development200: {
+      protocol: 'http',
+      host: '192.168.3.200',
       port: 8545,
       gas: 5000000,
       gasPrice: 5e9,
