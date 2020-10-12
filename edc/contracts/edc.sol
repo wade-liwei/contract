8a4a276ec702f285a1b40176d63ec9cbf6cea5a7
wade <wade-liwei@icloud.com>
2020-10-12 15:36:27 +0800

    fix supply

--- edc/contracts/edc.sol
+++ edc/contracts/edc.sol
@@ -12,7 +12,6 @@ contract EDC is ERC20 {
         // Similar to how
         // 1 dollar = 100 cents
         // 1 token = 1 * (10 ** decimals)
-        //_mint(msg.sender, 100 * 10 ** uint(decimals()));
-        _mint(msg.sender, 5 * 100000000 * uint(decimals()));
+        _mint(msg.sender, 500000000 * 10 ** uint(decimals()));
     }
 }
