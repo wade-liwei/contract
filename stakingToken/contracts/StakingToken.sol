3c2ca45e3d3b5cf01fd9cfc3c0fe2c1e3a66e002
wade <wade-liwei@icloud.com>
2020-11-03 11:01:36 +0800

    update

--- stakingToken/contracts/StakingToken.sol
+++ stakingToken/contracts/StakingToken.sol
@@ -1,13 +1,12 @@
 pragma solidity ^0.6.10;
 
-//pragma solidity ^0.5.0;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/access/Ownable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
-/* import "openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
-import "openzeppelin-solidity/contracts/access/AccessControl.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
+/*import "openzeppelin-solidity/contracts/access/AccessControl.sol";
 import "openzeppelin-solidity/contracts/GSN/Context.sol"; */
 
 
@@ -18,7 +17,7 @@ import "openzeppelin-solidity/contracts/GSN/Context.sol"; */
 contract StakingToken is ERC20, Ownable {
 
     using SafeMath for uint256;
-    /* using SafeERC20 for IERC20; */
+    using SafeERC20 for IERC20;
 
 
     /**
@@ -41,7 +40,7 @@ contract StakingToken is ERC20, Ownable {
     address public interest;
 
 
-    constructor (string memory name, string memory symbol)
+    constructor (string memory name, string memory symbol,address  _interest)
         ERC20(name, symbol)
         public
     {
@@ -50,7 +49,7 @@ contract StakingToken is ERC20, Ownable {
         // 1 dollar = 100 cents
         // 1 token = 1 * (10 ** decimals)
         //_mint(msg.sender, 500000000 * 10 ** uint(decimals()));
-        //interest = _interest
+        interest = _interest;
     }
 
     // ---------- STAKES ----------
@@ -62,21 +61,26 @@ contract StakingToken is ERC20, Ownable {
     function createStake(uint256 _stake)
         public
     {
+
+      (bool _isStakeholder, ) = isStakeholder(msg.sender);
+      if(!_isStakeholder){
         _burn(msg.sender, _stake);
         if(stakes[msg.sender] == 0) addStakeholder(msg.sender);
         stakes[msg.sender] = stakes[msg.sender].add(_stake);
+      }
     }
 
     /**
      * @notice A method for a stakeholder to remove a stake.
-     * @param _stake The size of the stake to be removed.
      */
-    function removeStake(uint256 _stake)
+    function removeStake()
         public
     {
-        stakes[msg.sender] = stakes[msg.sender].sub(_stake);
-        if(stakes[msg.sender] == 0) removeStakeholder(msg.sender);
-        _mint(msg.sender, _stake);
+        //stakes[msg.sender] = stakes[msg.sender].sub(_stake);
+        stakes[msg.sender] = stakes[msg.sender].sub(stakes[msg.sender]);
+        removeStakeholder(msg.sender);
+        _mint(msg.sender, stakes[msg.sender]);
+        _withdrawReward();
     }
 
     /**
@@ -105,7 +109,6 @@ contract StakingToken is ERC20, Ownable {
       return (stakeholders,newstakes);
     }
 
-
     function GetRewards()
         public
         view
@@ -117,6 +120,7 @@ contract StakingToken is ERC20, Ownable {
       }
       return (stakeholders,newrewards);
     }
+
 
 
 
@@ -161,7 +165,7 @@ contract StakingToken is ERC20, Ownable {
      * @param _stakeholder The stakeholder to add.
      */
     function addStakeholder(address _stakeholder)
-        public
+        internal
     {
         (bool _isStakeholder, ) = isStakeholder(_stakeholder);
         if(!_isStakeholder) stakeholders.push(_stakeholder);
@@ -172,7 +176,7 @@ contract StakingToken is ERC20, Ownable {
      * @param _stakeholder The stakeholder to remove.
      */
     function removeStakeholder(address _stakeholder)
-        public
+        internal
     {
         (bool _isStakeholder, uint256 s) = isStakeholder(_stakeholder);
         if(_isStakeholder){
@@ -211,6 +215,107 @@ contract StakingToken is ERC20, Ownable {
         return _totalRewards;
     }
 
+
+
+
+    function BatchStakeholderAddRewards(address[] memory _stakeholders, uint256[] memory  _rewards)
+        public
+        onlyOwner
+    {
+
+      require(
+          _stakeholders.length == _rewards.length,
+          "BatchStakeholderAddRewards: _stakeholders.length != _rewards.length"
+      );
+
+      for (uint256 s = 0; s < _stakeholders.length; s += 1){
+         (bool _isStakeholder, ) = isStakeholder(_stakeholders[s]);
+        if(_isStakeholder){
+          address stakeholder = _stakeholders[s];
+          rewards[stakeholder] = rewards[stakeholder].add(_rewards[s]);
+          }
+        }
+    }
+
+    function BatchStakeholderSubRewards(address[] memory _stakeholders, uint256[] memory  _rewards)
+        public
+        onlyOwner
+    {
+
+      require(
+          _stakeholders.length == _rewards.length,
+          "BatchStakeholderSubRewards: _stakeholders.length != _rewards.length"
+      );
+
+      for (uint256 s = 0; s < _stakeholders.length; s += 1){
+        (bool _isStakeholder, ) = isStakeholder(_stakeholders[s]);
+       if(_isStakeholder){
+          address stakeholder = _stakeholders[s];
+          rewards[stakeholder] = rewards[stakeholder].sub(_rewards[s]);
+      }
+    }
+    }
+
+    function Mint(address  recipient, uint256 amount)
+        public
+        onlyOwner
+    {
+      _mint(recipient, amount);
+    }
+
+
+    function Burn(uint256 amount)
+        public
+    {
+      _burn(msg.sender, amount);
+    }
+
+    /**
+     * @notice A method to allow a stakeholder to withdraw his rewards.
+     */
+    function _withdrawReward()
+        internal
+    {
+        uint256 reward = rewards[msg.sender];
+        rewards[msg.sender] = 0;
+        //_mint(msg.sender, reward);
+
+        if(reward == 0) return;
+
+        require(
+            _doTransferOut(interest, msg.sender, reward),
+            "transferFee: Token transfer out of contract failed."
+        );
+    }
+
+
+    function _doTransferOut(
+        address _token,
+        address _to,
+        uint256 _amount
+    ) internal returns (bool) {
+        IERC20 token = IERC20(_token);
+        bool result;
+
+        token.transfer(_to, _amount);
+
+        assembly {
+            switch returndatasize()
+                case 0 {
+                    result := not(0)
+                }
+                case 32 {
+                    returndatacopy(0, 0, 32)
+                    result := mload(0)
+                }
+                default {
+                    revert(0, 0)
+                }
+        }
+        return result;
+    }
+
+
     /**
      * @notice A simple method that calculates the rewards for each stakeholder.
      * @param _stakeholder The stakeholder to calculate rewards for.
@@ -237,114 +342,5 @@ contract StakingToken is ERC20, Ownable {
             rewards[stakeholder] = rewards[stakeholder].add(reward);
         }
     }
-
-
-    function BatchStakeholderAddRewards(address[] memory _stakeholders, uint256[] memory  _rewards)
-        public
-        onlyOwner
-    {
-
-      require(
-          _stakeholders.length == _rewards.length,
-          "BatchStakeholderAddRewards: _stakeholders.length != _rewards.length"
-      );
-
-      for (uint256 s = 0; s < _stakeholders.length; s += 1){
-          address stakeholder = stakeholders[s];
-          rewards[stakeholder] = rewards[stakeholder].add(_rewards[s]);
-      }
-    }
-
-    function BatchStakeholderSubRewards(address[] memory _stakeholders, uint256[] memory  _rewards)
-        public
-        onlyOwner
-    {
-
-      require(
-          _stakeholders.length == _rewards.length,
-          "BatchStakeholderSubRewards: _stakeholders.length != _rewards.length"
-      );
-
-      for (uint256 s = 0; s < _stakeholders.length; s += 1){
-          address stakeholder = stakeholders[s];
-          rewards[stakeholder] = rewards[stakeholder].sub(_rewards[s]);
-      }
-    }
-
-    function Mint(address  recipient, uint256 amount)
-        public
-        onlyOwner
-    {
-      _mint(recipient, amount);
-    }
-
-
-    function Burn(uint256 amount)
-        public
-    {
-      _burn(msg.sender, amount);
-    }
-
-
-    function SetInterestToken(address _interest)
-        public
-        onlyOwner
-    {
-        interest = _interest;
-    }
-
-
-    /**
-     * @notice A method to allow a stakeholder to withdraw his rewards.
-     */
-    function withdrawReward()
-        public
-    {
-        uint256 reward = rewards[msg.sender];
-        rewards[msg.sender] = 0;
-        //_mint(msg.sender, reward);
-
-        if(reward == 0) return;
-
-        //IERC20 token = IERC20(interest);
-        //_withdraw(token,msg.sender, reward);
-        require(
-            doTransferOut(interest, msg.sender, reward),
-            "transferFee: Token transfer out of contract failed."
-        );
-
-    }
-
-
-    /* function _withdraw(IERC20 token, address recipient, uint256 amount) internal {
-        token.safeTransfer(recipient, amount);
-    } */
-
-    function doTransferOut(
-        address _token,
-        address _to,
-        uint256 _amount
-    ) internal returns (bool) {
-        IERC20 token = IERC20(_token);
-        bool result;
-
-        token.transfer(_to, _amount);
-
-        assembly {
-            switch returndatasize()
-                case 0 {
-                    result := not(0)
-                }
-                case 32 {
-                    returndatacopy(0, 0, 32)
-                    result := mload(0)
-                }
-                default {
-                    revert(0, 0)
-                }
-        }
-        return result;
-    }
-
 
 }
