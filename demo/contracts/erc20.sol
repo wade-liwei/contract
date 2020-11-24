8759ddebfed05142c39aaa266284fc199a57e11e
wade <wade-liwei@icloud.com>
2020-11-24 15:57:28 +0800

    add erc20

---
+++ demo/contracts/erc20.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.6.10;
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
+
+contract StdErc20 is ERC20 {
+    constructor (string memory name, string memory symbol)
+        ERC20(name, symbol)
+        public
+    {
+
+        // Mint 100 tokens to msg.sender
+        // Similar to how
+        // 1 dollar = 100 cents
+        // 1 token = 1 * (10 ** decimals)
+        //_setupDecimals(10)
+        _mint(msg.sender, 10000000 * 10 ** uint(decimals()));
+    }
+}
